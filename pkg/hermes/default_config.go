5ef254aab58e8b2818c57c17fab80ab86b46d883
Darren Hague <d.hague@sap.com>
2017-05-08 10:21:35 +0100

    Correct default keystone driver

--- pkg/hermes/default_config.go
+++ pkg/hermes/default_config.go
@@ -3,6 +3,6 @@ package hermes
 import "github.com/spf13/viper"
 
 func SetDefaultConfig() {
-	viper.SetDefault("hermes.keystone_driver", "real")
+	viper.SetDefault("hermes.keystone_driver", "keystone")
 	viper.SetDefault("API.ListenAddress", "0.0.0.0:8788")
 }
