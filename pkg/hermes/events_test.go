9c3c406cc95c3de0ea3a3c28ddc4298b1bb77a4c
Darren Hague <d.hague@sap.com>
2017-05-09 11:36:06 +0100

    Fix events test

--- pkg/hermes/events_test.go
+++ pkg/hermes/events_test.go
@@ -5,24 +5,31 @@ import (
 
 	"github.com/sapcc/hermes/pkg/data"
 	"github.com/sapcc/hermes/pkg/storage"
+	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+func setup() {
+	viper.Set("hermes.keystone_driver", "mock")
+	viper.Set("hermes.storage_driver", "mock")
+}
+
 func Test_GetEvent(t *testing.T) {
-	storage := storage.Mock()
+	setup()
 	eventId := "d5eed458-6666-58ec-ad06-8d3cf6bafca1"
-	event, err := GetEvent(eventId, storage)
+	event, err := GetEvent(eventId, storage.ConfiguredDriver())
 	require.Nil(t, err)
 	require.NotNil(t, event)
-	assert.Equal(t, event.ID, "d5eed458-6666-58ec-ad06-8d3cf6bafca1")
-	assert.NotEmpty(t, event.Type)
-	assert.NotEmpty(t, event.Time)
+	assert.Equal(t, event.Payload.ID, "d5eed458-6666-58ec-ad06-8d3cf6bafca1")
+	assert.NotEmpty(t, event.Payload.EventType)
+	assert.NotEmpty(t, event.Payload.EventTime)
+	assert.NotEmpty(t, event.Payload.Target.Name)
 }
 
 func Test_GetEvents(t *testing.T) {
-	storage := storage.Mock()
-	events, total, err := GetEvents(storage, data.Filter{})
+	setup()
+	events, total, err := GetEvents(storage.ConfiguredDriver(), data.Filter{})
 	require.Nil(t, err)
 	require.NotNil(t, events)
 	assert.Equal(t, len(events), 3)
