b7e854836f86383c8f460efc190411ddc356791a
Nathan Oyler <notque@gmail.com>
2017-12-05 12:00:10 -0700

    fix mock storage tests

--- pkg/storage/mock_test.go
+++ pkg/storage/mock_test.go
@@ -9,18 +9,19 @@ func Test_MockStorage_EventDetail(t *testing.T) {
 	eventDetail, error := Mock{}.GetEvent("d5eed458-6666-58ec-ad06-8d3cf6bafca1", "b3b70c8271a845709f9a03030e705da7")
 
 	assert.Nil(t, error)
-	assert.Equal(t, "d5eed458-6666-58ec-ad06-8d3cf6bafca1", eventDetail.Payload.ID)
-	assert.Equal(t, "identity.project.deleted", eventDetail.EventType)
-	assert.Equal(t, "2017-05-02T12:02:46.726056+0000", eventDetail.Payload.EventTime)
+	assert.Equal(t, "7be6c4ff-b761-5f1f-b234-f5d41616c2cd", eventDetail.ID)
+	assert.Equal(t, "create/role_assignment", eventDetail.Action)
+	assert.Equal(t, "2017-11-17T08:53:32.667973+0000", eventDetail.EventTime)
+	assert.Equal(t, "success", eventDetail.Outcome)
 }
 
 func Test_MockStorage_Events(t *testing.T) {
 	eventsList, total, error := Mock{}.GetEvents(&Filter{}, "b3b70c8271a845709f9a03030e705da7")
 
 	assert.Nil(t, error)
-	assert.Equal(t, total, 24)
-	assert.Equal(t, len(eventsList), 3)
-	assert.Equal(t, "identity.project.deleted", eventsList[0].EventType)
-	assert.Equal(t, "095056c9-4cbb-5200-af70-0977dbcf5000", eventsList[1].Payload.ID)
-	assert.Equal(t, "2017-05-02T11:45:44.755215+0000", eventsList[2].Payload.EventTime)
+	assert.Equal(t, total, 4)
+	assert.Equal(t, len(eventsList), 4)
+	assert.Equal(t, "success", eventsList[0].Outcome)
+	assert.Equal(t, "f6f0ebf3-bf59-553a-9e38-788f714ccc46", eventsList[1].ID)
+	assert.Equal(t, "2017-11-06T10:15:56.984390+0000", eventsList[2].EventTime)
 }
