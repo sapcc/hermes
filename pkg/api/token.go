eb3c88abd1fbe6af59b16a11560db36553a33eef
Nathan Oyler <notque@gmail.com>
2020-01-09 12:51:48 -0700

    Revert "remove filling details with token context"
    This reverts commit 33465c5ceb8da9f9d3620e9c35c016b8d892e523.

--- pkg/api/token.go
+++ pkg/api/token.go
@@ -59,10 +59,14 @@ func (p *v1Provider) CheckToken(r *http.Request) *Token {
 		}
 	}
 	t.context.Request = mux.Vars(r)
-	if r.FormValue("domain_id") != "" {
+	if r.FormValue("domain_id") == "" {
+		t.context.Request["domain_id"] = t.context.Auth["domain_id"]
+	} else {
 		t.context.Request["domain_id"] = r.FormValue("domain_id")
 	}
-	if r.FormValue("project_id") != "" {
+	if r.FormValue("project_id") == "" {
+		t.context.Request["project_id"] = t.context.Auth["project_id"]
+	} else {
 		t.context.Request["project_id"] = r.FormValue("project_id")
 	}
 	return t
