437b9b74360b632a15f16d4753e1031fc90799de
Nathan Oyler <notque@gmail.com>
2023-10-19 11:03:07 -0700

    add logging for search filtering

--- internal/storage/elasticsearch.go
+++ internal/storage/elasticsearch.go
@@ -168,6 +168,7 @@ func (es ElasticSearch) GetEvents(filter *EventFilter, tenantID string) ([]*cadf
 
 	// Check if a search string is provided in EventFilter
 	if filter.Search != "" {
+		logg.Debug("Filtering on Search %s", filter.Search)
 		// Create a Query String Query for global text search
 		queryStringQuery := elastic.NewQueryStringQuery(filter.Search)
 		// Combine the Query String Query with the existing Bool Query
