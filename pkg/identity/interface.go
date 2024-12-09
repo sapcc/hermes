f69f95a3c0459917b071224b47fb3980602aa707
Stefan Majewsky <stefan.majewsky@sap.com>
2024-12-09 21:20:17 +0100

    cleanup unused code
    The identity.Identity interface caught my eye and I wondered how many of
    its functions are used. As it turns out, only one. When I started
    pulling, a whole lot of stuff fell out that evidently is not used anymore.

--- pkg/identity/interface.go
+++ pkg/identity/interface.go
@@ -23,25 +23,11 @@ import (
 	"context"
 
 	policy "github.com/databus23/goslo.policy"
-	"github.com/gophercloud/gophercloud/v2"
 )
 
 // Identity is an interface that wraps the authentication of the service user and
 // token checking of API users. Because it is an interface, the real implementation
 // can be mocked away in unit tests.
 type Identity interface {
-	// Return the main gophercloud client from which the respective service
-	// clients can be derived. For Mock drivers, this returns nil, so test code
-	// should be prepared to handle a nil Client() where appropriate.
-	Client() (*gophercloud.ProviderClient, error)
-	AuthOptions() gophercloud.AuthOptions
-	/********** requests to Keystone **********/
 	ValidateToken(ctx context.Context, token string) (policy.Context, error)
-	Authenticate(ctx context.Context, credentials gophercloud.AuthOptions) (policy.Context, error)
-	DomainName(ctx context.Context, id string) (string, error)
-	ProjectName(ctx context.Context, id string) (string, error)
-	UserName(ctx context.Context, id string) (string, error)
-	UserID(ctx context.Context, name string) (string, error)
-	RoleName(ctx context.Context, id string) (string, error)
-	GroupName(ctx context.Context, id string) (string, error)
 }
